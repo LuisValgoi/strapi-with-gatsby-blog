c3d7e9e5086fbe792cbcabdcfed785a5fe34e2e8
Luis Valgoi <luisvalgoi@gmail.com>
2021-05-14 00:48:04 -0300

    chore: added apirulconfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,9 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: process.env.DEPLOY_URL
+          ? "https://strapi-with-gatsby-cms.herokuapp.com"
+          : `http://localhost:1337`,
         contentTypes: [`article`, `user`],
         queryLimit: 1000,
       },
